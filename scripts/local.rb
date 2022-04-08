2962b8030d1ef03e5edf1690350acfca6f7166ca
RutvikChandla <rutvik.c@browserstack.com>
2022-04-08 09:02:42 +0530

    Selenium - 4 changes

---
+++ scripts/local.rb
@@ -0,0 +1,50 @@
+require 'rubygems'
+require 'selenium-webdriver'
+require "browserstack/local"
+
+# Creates an instance of Local
+bs_local = BrowserStack::Local.new
+
+# You can also set an environment variable - "BROWSERSTACK_ACCESS_KEY".
+bs_local_args = { "key" => "ACCESS_KEY" }
+
+# Starts the Local instance with the required arguments
+bs_local.start(bs_local_args)
+
+# Check if BrowserStack local instance is running
+puts bs_local.isRunning
+# Input capabilities
+caps = Selenium::WebDriver::Remote::Capabilities.new
+caps['device'] = 'iPhone 11'
+caps['realMobile'] = 'true'
+caps['os_version'] = '14.0'
+caps['javascriptEnabled'] = 'true'
+caps['browserstack.local'] = 'true'
+caps['name'] = 'BStack-[Ruby] Sample Test' # test name
+caps['build'] = 'BStack Build Number 1' # CI/CD job or build name
+driver = Selenium::WebDriver.for(:remote,
+  :url =>  "https://USER_NAME:ACCESS_KEY@hub-cloud.browserstack.com/wd/hub",
+  :desired_capabilities => caps)
+begin
+    # opening the bstackdemo.com website
+    driver.navigate.to "http://bs-local.com:45691/check"
+    wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
+    
+    body = driver.find_element(:css, 'body')
+    wait.until { body.displayed? }
+    body_text = body.text
+
+    if body_text.eql? "Up and running"
+        # marking test as 'passed' local ran succesfully
+        driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "Local ran successfully"}}')
+    else
+        # marking test as 'failed' 
+        driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Local setup failed"}}')
+    end
+# marking test as 'failed' if test script is unable to open the local website
+    driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Some elements failed to load"}}')
+end
+driver.quit 
+
+  # Stop the Local instance
+  bs_local.stop
---
+++ scripts/single.rb
@@ -0,0 +1,51 @@
+require 'rubygems'
+require 'selenium-webdriver'
+
+options = Selenium::WebDriver::Options.chrome
+options.browser_version = 'latest'
+options.platform_name = 'MAC'
+bstack_options = {
+    "os" => "OS X",
+    "osVersion" => "Sierra",
+    "buildName" => "Final-Snippet-Test",
+    "sessionName" => "Selenium-4 Ruby snippet test",
+    "local" => "false",
+    "seleniumVersion" => "4.0.0",
+}
+options.add_option('bstack:options', bstack_options)
+
+driver = Selenium::WebDriver.for(:remote,
+  :url => "https://YOUR_USER_NAME:YOUR_ACCESS_KEY@hub-cloud.browserstack.com/wd/hub",
+  :capabilities => options)
+begin
+    # opening the bstackdemo.com website
+    driver.navigate.to "https://bstackdemo.com"
+    wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
+    wait.until { !driver.title.match(/StackDemo/i).nil? }
+    # getting name of the product available on the webpage
+    product = driver.find_element(:xpath, '//*[@id="1"]/p')
+    wait.until { product.displayed? }
+    product_text = product.text
+    # waiting until the Add to Cart button is displayed on webpage and then clicking it
+    cart_btn = driver.find_element(:xpath, '//*[@id="1"]/div[4]')
+    wait.until { cart_btn.displayed? }
+    cart_btn.click
+    # waiting until the Cart pane appears
+    wait.until { driver.find_element(:xpath, '//*[@id="__next"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]').displayed? }
+    # getting name of the product in the cart
+    product_in_cart = driver.find_element(:xpath, '//*[@id="__next"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]')
+    wait.until { product_in_cart.displayed? }
+    product_in_cart_text = product_in_cart.text
+    # checking if the product has been added to the cart
+    if product_text.eql? product_in_cart_text
+        # marking test as 'passed' if the product is successfully added to the cart
+        driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "Product has been successfully added to the cart!"}}')
+    else
+        # marking test as 'failed' if the product is not added to the cart
+        driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Failed to add product to the cart"}}')
+    end
+# marking test as 'failed' if test script is unable to open the bstackdemo.com website
+rescue
+    driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Some elements failed to load"}}')
+end
+driver.quit 
