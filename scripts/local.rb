e5eb0b180d7dae20b38057ee2e8c2c255bfbd7c6
RutvikChandla <rutvik.c@browserstack.com>
2022-04-12 10:02:24 +0530

    change 2.0

--- scripts/local.rb
+++ scripts/local.rb
@@ -2,17 +2,20 @@ require 'rubygems'
 require 'selenium-webdriver'
 require "browserstack/local"
 
+USER_NAME = ENV['BROWSERSTACK_USER_NAME'] || "YOUR_USER_NAME"
+ACCESS_KEY = ENV['BROWSERSTACK_ACCESS_KEY'] || "YOUR_ACCESS_KEY"
+
 # Creates an instance of Local
 bs_local = BrowserStack::Local.new
 
 # You can also set an environment variable - "BROWSERSTACK_ACCESS_KEY".
-bs_local_args = { "key" => "ACCESS_KEY" }
+bs_local_args = { "key" => ACCESS_KEY }
 
 # Starts the Local instance with the required arguments
 bs_local.start(bs_local_args)
 
 # Check if BrowserStack local instance is running
-puts bs_local.isRunning
+puts "Local running: #{bs_local.isRunning}"
 # Input capabilities
 caps = Selenium::WebDriver::Remote::Capabilities.new
 caps['device'] = 'iPhone 11'
@@ -22,8 +25,7 @@ caps['javascriptEnabled'] = 'true'
 caps['browserstack.local'] = 'true'
 caps['name'] = 'BStack-[Ruby] Sample Test' # test name
 caps['build'] = 'BStack Build Number 1' # CI/CD job or build name
-USER_NAME = ENV['BROWSERSTACK_USER_NAME'] || "YOUR_USER_NAME"
-ACCESS_KEY = ENV['BROWSERSTACK_ACCESS_KEY'] || "YOUR_ACCESS_KEY"
+
 driver = Selenium::WebDriver.for(:remote,
   :url => "https://#{USER_NAME}:#{ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub",
   :desired_capabilities => caps)
@@ -48,5 +50,5 @@ begin
 end
 driver.quit 
 
-  # Stop the Local instance
-  bs_local.stop
+# Stop the Local instance
+bs_local.stop
